2bfd17fc1d32023ad969e4e8acd2a7ab89913874
yangyang <1348379987@qq.com>
2026-02-24 04:21:33 +0000

    chore: Add `allowedDevOrigins` configuration to Next.js.

--- packages/nextjs/next.config.ts
+++ packages/nextjs/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   devIndicators: false,
+  allowedDevOrigins: ["*"],
   typescript: {
     ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
   },
